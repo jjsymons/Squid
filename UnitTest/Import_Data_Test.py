d724ffb5459afa1a2b70cae89c68d4dc2f698e4e
Joe Symons <joe.j.symons@gmail.com>
2024-04-01 11:49:12 +0100

    Renamed test files

--- UnitTest/Import_Data_Test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from Scripts import Import_Data
-
-def test_clean_data():
-    assert Import_Data.clean_data(Import_Data.import_data('data_test.csv')) == [{'Date': 20231028230000, 'kWh': 0.15}, {'Date': 20231028233000, 'kWh': 0.159}, {'Date': 20231029000000, 'kWh': 1.177}, {'Date': 20231029003000, 'kWh': 0.151}, {'Date': 20231029010000, 'kWh': 3.139}, {'Date': 20231029013000, 'kWh': 0.151}]
-
-
