a1b1c4d1f5021ac398ad030860add5d0196853d9
Joe Symons <joe.j.symons@gmail.com>
2024-04-11 20:29:42 +0100

    Worked on some timing to check between SQL query and python calculations and the SQL query was significantly slower.

--- Data/Data_Processing.py
+++ Data/Data_Processing.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from operator import itemgetter
 import os 
 import logging
+from timeit import default_timer as timer
 
 # File to take data requests to sqlite database and return them. #
 
@@ -15,14 +16,9 @@ def main():
     # Create log
     log_file = os.path.join(log_folder, 'data_processing.log')
     logging.basicConfig(filename=log_file, level=logging.INFO)
-    print(read_database(20230331230000, 20230401003000))
     data = read_database(20230331230000, 20230401003000)
 
-    print(f"""Max: {find_max(data)}
-          \nMin: {find_min(data)}
-          \nAverage: {find_avg(data)}
-          \nSum: {find_sum(data)}""")
-    # Create Database if not found
+    print(f"""Max: {find_max(data)}\nMin: {find_min(data)}\nAverage: {find_avg(data)}\nSum: {find_sum(data)}\n""")
 
 # Function for opening the database
 
@@ -63,17 +59,48 @@ def find_sum(data):
         total += items[1]
     return total
 
+
+# Testing section:
+# Testing
+def test():
+    start = timer()
+    max = find_max()
+    end = timer()
+    total_time = end - start
+    print(f"Max: {total_time:.6f} seconds")
+
+    start = timer()
+    min = find_min(data)
+    end = timer()
+    total_time = end - start
+    print(f"Min: {total_time:.6f} seconds")
+
+    start = timer()
+    avg = find_avg(data)
+    end = timer()
+    total_time = end - start
+    print(f"Average: {total_time:.6f} seconds")
+
+    start = timer()
+    sum = find_sum(data)
+    end = timer()
+    total_time = end - start
+    print(f"Sum: {total_time:.6f} seconds")
+
+    # Outcomes from data 11/04/2024.
+    # SUMMARY
+    # Unless there is a more optimal solution that I have missed, the outcomes to the max find is very very slow. 
+    # 6 times slower. 0.000742 Seconds
+    #Max: (20231015120000, 2.558)
+    #Min: (20230430043000, 0.063)
+    #Average: 0.2651800102424021
+    #Sum: 3106.8489999999833
+
+    # Python code gives the following outputs on the full database:
+    #Max: 0.000131 seconds
+    #Min: 0.000130 seconds
+    #Average: 0.000213 seconds
+    #Sum: 0.000212 seconds
+
 if __name__ == '__main__':
     main()
-
-
-
-# funtion for reading the database with a specified time period
-
-
-
-
-
-
-    
-
