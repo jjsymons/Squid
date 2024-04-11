0bd3dc8b1aaf6467d3ca1539d7634b80ef007583
Joe Symons <joe.j.symons@gmail.com>
2024-04-11 17:55:42 +0100

    Added some calculations, removed some accidental duplications.

--- Data/Data_Processing.py
+++ Data/Data_Processing.py
@@ -2,6 +2,7 @@ import sqlite3
 import pandas as pd
 import Database_Methods as D_M
 from datetime import datetime
+from operator import itemgetter
 import os 
 import logging
 
@@ -10,13 +11,17 @@ import logging
 def main():
     log_folder = os.path.abspath('../Logs')
     os.makedirs(log_folder, exist_ok=True)
-    
-    log_folder = os.path.abspath('../Logs')
-    os.makedirs(log_folder, exist_ok=True)
+
     # Create log
     log_file = os.path.join(log_folder, 'data_processing.log')
-    logging.basicConfig(filename=log_file, level=logging.info)
+    logging.basicConfig(filename=log_file, level=logging.INFO)
     print(read_database(20230331230000, 20230401003000))
+    data = read_database(20230331230000, 20230401003000)
+
+    print(f"""Max: {find_max(data)}
+          \nMin: {find_min(data)}
+          \nAverage: {find_avg(data)}
+          \nSum: {find_sum(data)}""")
     # Create Database if not found
 
 # Function for opening the database
@@ -33,12 +38,30 @@ def read_database(start_date, end_date):
         logging.debug(f"Executing query with parameters: {start_date}, {end_date}")
         rows = cursor.fetchall()
         return rows
-    except sqlite3.Error as c:
-        logging.error(f"{datetime.now()}: Connection error: {c}")
+    except sqlite3.Error as e:
+        logging.error(f"{datetime.now()}: Sqlite error: {e}")
         return None
     finally:
         if conn:
             conn.close()
+
+def find_max(data):
+    return max(data, key = itemgetter(1))
+
+def find_min(data):
+    return min(data, key=itemgetter(1)) 
+
+def find_avg(data):
+    total = 0
+    for items in data:
+        total += items[1]
+    return total/len(data)
+    
+def find_sum(data):
+    total = 0
+    for items in data:
+        total += items[1]
+    return total
 
 if __name__ == '__main__':
     main()
